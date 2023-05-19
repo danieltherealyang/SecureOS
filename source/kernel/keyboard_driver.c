272b395a343865b5c6308bedfe757576b4ecaca6
r2dev2 <ronak.badhe@gmail.com>
2023-05-19 08:08:48 -0700

    keyboard driver interface

---
+++ source/kernel/keyboard_driver.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+#include "keyboard_driver.h"
+
+/**
+ * Get latest char that has not been gotten yet from keyboard event queue
+ */
+uint16_t get_char(void)
+{
+    return 0;
+}
